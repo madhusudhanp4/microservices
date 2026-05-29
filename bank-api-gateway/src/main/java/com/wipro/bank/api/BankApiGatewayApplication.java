f9bb1f2214476c0e49f59291ffdc1e12138246f9
madhusudhanp4 <madhusudhanp.4u@gmail.com>
2026-05-29 16:16:06 +0530

    Added API Gateway

---
+++ bank-api-gateway/src/main/java/com/wipro/bank/api/BankApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.wipro.bank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BankApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiGatewayApplication.class, args);
+    }
+}
---
+++ bank-api-gateway/src/test/java/com/wipro/bank/api/BankApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.bank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
